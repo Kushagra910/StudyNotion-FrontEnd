e2e561e3fe168e8f212628d6763ca13b77e3c31d
Kushagra910 <kushagram63@gmail.com>
2024-01-02 05:15:40 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route ,Routes} from "react-router-dom";
 import "./App.css";
 import Home  from "./pages/Home";
+import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900">
+      <Navbar/>
        <Routes>
-        <Route path="/" element={<Home/>}></Route>
+          <Route path="/" element={<Home/>}></Route>
        </Routes>
     </div>
   );
