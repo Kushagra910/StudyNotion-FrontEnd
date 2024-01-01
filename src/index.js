0004727b811b8ed71da8382469a6ce07f6431ffa
Kushagra910 <kushagram63@gmail.com>
2024-01-02 05:14:26 +0530

    Added Temporary store logic

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import rootReducer from "./reducer";
+import {configureStore} from "@reduxjs/toolkit"
 
+
+// temporary store
+const store = configureStore({
+  reducer:rootReducer,
+})
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
