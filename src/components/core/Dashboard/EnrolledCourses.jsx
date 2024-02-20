946443c99dad64dc2a60d8e501be3846204b8146
Kushagra910 <kushagram63@gmail.com>
2024-02-21 04:00:36 +0530

    Styling of enrolled courses page completed

--- src/components/core/Dashboard/EnrolledCourses.jsx
+++ src/components/core/Dashboard/EnrolledCourses.jsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { getUserEnrolledCourses } from "../../../services/operations/profileApi";
 import ProgressBar from "@ramonak/react-progress-bar";
+import { useNavigate } from "react-router-dom";
 
 const EnrolledCourses = () => {
   const { token } = useSelector((state) => state.auth);
   const [enrolledCourses, setEnrolledCourses] = useState(null);
+  const navigate = useNavigate();
 
   const getEnrolledCourses = async () => {
     try {
       const response = await getUserEnrolledCourses(token);
       setEnrolledCourses(response);
+      console.log("Enroll Courses", response);
     } catch (err) {
       console.log("Unable to Fetch Enrolled Courses Data");
     }
@@ -21,34 +24,54 @@ const EnrolledCourses = () => {
   }, []);
   return (
     <div className="text-white">
-      <div>Enrolled Courses</div>
+      <div className="text-3xl text-richblack-50">Enrolled Courses</div>
       {!enrolledCourses ? (
-        <div className="spinner"></div>
+        <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+          <div className="spinner"></div>
+        </div>
       ) : !enrolledCourses.length ? (
-        <p>You have not enrolled in any course yet</p>
+        <p className="grid h-[10vh] w-full place-content-center text-richblack-5">
+          You have not enrolled in any course yet
+        </p>
       ) : (
-        <div>
-          <div>
-            <div>Course Name</div>
-            <div>Durations</div>
-            <div>Progress</div>
+        <div className="my-8 text-richblack-5">
+          <div className="flex rounded-t-lg bg-richblack-500 ">
+            <div className="w-[45%] px-5 py-3">Course Name</div>
+            <div className="w-1/4 px-2 py-3">Durations</div>
+            <div className="flex-1 px-2 py-3">Progress</div>
           </div>
           {/* Card Section */}
-          {enrolledCourses.map((course, idx) => {
+          {enrolledCourses.map((course, idx, arr) => {
             return (
-              <div key={idx}>
-                <div>
-                  <img src={course.thumbnail} />
-                  <div>
-                    <p>{course.courseName}</p>
+              <div
+                key={idx}
+                className={`flex items-center border border-richblack-700 ${
+                  idx === arr.length - 1 ? "rounded-b-lg" : "rounded-none"
+                }`}
+              >
+                <div
+                  onClick={() => {
+                    navigate(
+                      `/view-course/${course?._id}/section/${course.courseContent?.[0]?._id}/sub-section/${course.courseContent?.[0]?.subSection?.[0]?._id}`
+                    );
+                  }}
+                  className="flex w-[45%] cursor-pointer items-center gap-4 px-5 py-3"
+                >
+                  <img src={course.thumbnail}
+                                    alt="courseImg"
+                  className="h-14 w-14 rounded-lg object-cover" />
+                  <div className="flex max-w-xs flex-col gap-2">
+                    <p className="font-semibold">{course.courseName}</p>
                     {/* conditional rendering on description */}
-                    <p>{course.courseDescription}</p>
+                    <p className="text-xs text-richblack-300">                    {course.courseDescription.length > 50
+                      ? `${course.courseDescription.slice(0, 50)}...`
+                      : course.courseDescription}</p>
                   </div>
                 </div>
                 {/* duration section */}
-                <div>{course?.totalDuration}</div>
+                <div  className="w-1/4 px-2 py-3">{course?.totalDuration}</div>
                 {/* Progress Section */}
-                <div>
+                <div className="flex w-1/5 flex-col gap-2 px-2 py-3">
                   <p>Progress:{course.progressPercentage || 0}%</p>
                   {/* progress bar  third party package -> ramonak/progress-bar*/}
                   <ProgressBar
